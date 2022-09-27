98cdc9283670806889d4f47094f8c90c2d813d87
Aryan Deshpande <deshpandearyan1@gmail.com>
2022-09-27 15:23:07 +0530

    torch deployment

--- TorchDeployment/deployment.py
+++ TorchDeployment/deployment.py
@@ -1,6 +1,6 @@
 from crypt import methods
 from flask import Flask, Blueprint
-from flask import render_template, redirect, jsonify, request
+from flask import render_template, redirect, jsonify, request, abort
 
 app = Flask(__name__)
 
@@ -9,10 +9,24 @@ def predict():
     pass
 
 
-# Adding blueprint functionality
-model_api = Blueprint('model_api', __name__, url_prefix='/model')
-app.register_blueprint(model_api)
+# Adding blueprint functionality, used for organizing routes ( http://localhost:8080/api/members) => gives an error 
 
+# Parent API
+api = Blueprint('api', __name__, url_prefix='/api')
+
+# Child API
+model_api = Blueprint('model_api', __name__, url_prefix='/model')
+
+app.register_blueprint(api)
+api.register_blueprint(model_api)
+
+@model_api.route('/')
+def afunction():
+    abort(404)
+#    | |
+#    | |
+#    | |
+#     v
 @model_api.errorhandler(404)
 def errhandler(err):
     return 'there is an error'
